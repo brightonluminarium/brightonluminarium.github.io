fd693c28b6f5ca72547f8160abcf0e27a1b280d9
DBaskill <97393103+DBaskill@users.noreply.github.com>
2022-04-13 23:43:22 +0100

    Update slideshow.js

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,103 +1,30 @@
-
+'use strict';
 var slideIndex = 1;
-var t;
-var dots;
-var maxheight = 0;
-var timedelay = 5000;
 
-window.onload = function() {
-
-var x = document.getElementsByClassName("mySlides");
-var parent_elem = document.getElementById('myslideFrame')
-
-var widthRatio = parseInt(parent_elem.offsetWidth) / parseInt(x[0].width)
-
-for (i = 0; i < x.length; i++) {
-
-// x.style.width = "100%";
-
-  if (x.height > maxheight) {
-    maxheight = x.height;
-  }
-
-}
- 
-
-document.getElementById('myslideFrame').style.height = maxheight * widthRatio + "px";
-
-  showDivs(slideIndex);
-
-carousel();
-
-};
-
- 
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
- 
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
- 
-
-function showDivs(n) {
-
+function showSlides(n) {
+  alert('showslides('+ n +')\nslideIndex='+slideIndex);
   var i;
-  var x = document.getElementsByClassName("mySlides");
-
-  dots = document.getElementsByClassName("demo");
-
-  if (n > x.length) {
-    slideIndex = 1;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n==undefined){n = ++slideIndex}
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-
-  for (i = 0; i < x.length; i++) {
-    x.style.display = "none";
-  }
-
   for (i = 0; i < dots.length; i++) {
-    dots.className = dots.className.replace(" w3-border-red", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-
-  x[slideIndex - 1].style.display = "inline-block";
-  dots[slideIndex - 1].className += " w3-border-red";
-} 
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-
-  for (i = 0; i < x.length; i++) {
-    x.style.display = "none";
-    dots.className = dots.className.replace(" w3-border-red", "");
-  }
-
-  slideIndex++;
-
-  if (slideIndex > x.length) {  
-    slideIndex = 1
-  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
   
-  x[slideIndex - 1].style.display = "inline-block";
-  dots[slideIndex - 1].className += " w3-border-red";
-  t = setTimeout(carousel, timedelay);
-  
-}
-
-
-function pauseCarousel() {
-  clearTimeout(t)
-}
-
-function startCarousel() {
-  t = setTimeout(carousel, timedelay);
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
